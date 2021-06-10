bc6947140b98b072bbfd54d5f8541212a241c6ae
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-06-10 18:08:35 +0530

    fixed day 5.1.1.py

--- day5.1.py
+++ day5.1.py
@@ -2,10 +2,14 @@
 heights=input("Enter heights of people").split()
 print(heights)
 i=0
+sum=0
 for n in range(0,len(heights)):
     heights[n]=int(heights[n])
     sum=sum+heights[n]
-    i+=1
-print(f'Number of people ={i}')
-    
+    i=i+1
+print(f'Number of people: {i}')
+print(f'sum of heights:{sum}')
+avg=sum/i
+print(f'Average height of people {avg}')
+
 
