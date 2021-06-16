a99c2c1d790dc3e61710e4603ae98d7a21718c48
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-06-16 15:29:53 +0530

    Corrected working

--- day7.py
+++ day7.py
@@ -1,12 +1,80 @@
 #hangman game
 import random
+stages = ['''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ / \  |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+ /    |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|\  |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+ /|   |
+      |
+      |
+=========''', '''
+  +---+
+  |   |
+  O   |
+  |   |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+  O   |
+      |
+      |
+      |
+=========
+''', '''
+  +---+
+  |   |
+      |
+      |
+      |
+      |
+=========
+''']
 word_list=['andaman','nicobar','island']
 chosen_word=random.choice(word_list)
+lis=list(chosen_word)
+print(lis)
 print(chosen_word)
-inp=input("Enter a character")
-inp.lower()
+display=[]
 for i in range(0,len(chosen_word)):
-    if(chosen_word[i]!=inp):
-        print("__",end=" ")
-    else:
-        print(inp,end=' ')
+    display+=['']
+k=0
+while(k<=len(chosen_word)):
+    k=k+1
+    inp=input("Enter letter: ").lower()
+    for j in range(0,len(chosen_word)):
+        if(chosen_word[j]==inp):
+            display[j]=inp
+            print(display)
+
+if('' not in display):
+    print("You Win")
+else:
+    print("You loose")
+print(f'Correct word is {chosen_word}')
