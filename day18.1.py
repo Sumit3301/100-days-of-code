56946db20dc667961fc47b259c8608692ca6b1b6
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-07-14 17:33:23 +0530

    added day 18.2

---
+++ day18.1.py
@@ -0,0 +1,19 @@
+#Making a squrae using turtle
+from turtle import Turtle,Screen
+
+timmy=Turtle()
+timmy.shape("turtle")
+timmy.color("red")
+
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+timmy.forward(100)
+timmy.left(90)
+
+
+screen= Screen()
+screen.exitonclick()
---
+++ day18.2.py
@@ -0,0 +1,20 @@
+#dashed line
+from turtle import Turtle, Screen
+timmy=Turtle()
+
+timmy.forward(10)
+timmy.penup()
+timmy.forward(10)
+timmy.pendown()
+timmy.forward(10)
+timmy.penup()
+timmy.forward(10)
+timmy.pendown()
+timmy.forward(10)
+timmy.penup()
+timmy.forward(10)
+timmy.pendown()
+
+
+screen= Screen()
+screen.exitonclick()
