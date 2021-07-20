545c6f0e9fdc980b8d5e130455ae3e77df1a1da6
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-07-21 00:12:29 +0530

    Finaly day 18

--- day18.5.py
+++ day18.5.py
@@ -1,3 +1,4 @@
+#Dot Painting
 import turtle
 import colorgram
 import random
@@ -5,6 +6,7 @@ from turtle import Turtle,Screen
 rgb_colors=[]
 turtle.colormode(255)
 timmy=turtle.Turtle()
+timmy.speed("fastest")
 """colors = colorgram.extract('damien.jpg', 30)
 for color in colors:
     r=color.rgb.r
@@ -15,16 +17,25 @@ for color in colors:
 print(rgb_colors)"""
 
 color_list=[(233, 239, 246), (246, 239, 242), (240, 246, 243), (132, 166, 205), (221, 148, 106), (32, 42, 61), (199, 135, 148), (166, 58, 48), (141, 184, 162), (39, 105, 157), (237, 212, 90), (150, 59, 66), (216, 82, 71), (168, 29, 33), (235, 165, 157), (51, 111, 90), (35, 61, 55), (156, 33, 31), (17, 97, 71), (52, 44, 49), (230, 161, 166), (170, 188, 221), (57, 51, 48), (184, 103, 113), (32, 60, 109), (105, 126, 159), (175, 200, 188), (34, 151, 210), (65, 66, 56)]
+timmy.penup()
+timmy.setheading(235)
+timmy.forward(300)
+timmy.setheading(0)
 
-for i in range(10):
+dots_number=100
+
+for dot_count in range(1,dots_number+1):
     timmy.dot(20,random.choice(color_list))
     timmy.forward(50)
+    if(dot_count%10==0):
+        timmy.setheading(90)
+        timmy.forward(50)
+        timmy.setheading(180)
+        timmy.forward(500)
+        timmy.setheading(0)
 
-timmy.setheading(90)
-timmy.forward(50)
-timmy.setheading(180)
-timmy.forward(500)
-
+screen= Screen()
+screen.exitonclick()
 
 
 
