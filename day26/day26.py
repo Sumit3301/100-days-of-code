99929c1e4496b6d7266a35212bc5a8e17decdfd9
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-08-02 01:46:00 +0530

    reading files

--- day26/day26.py
+++ day26/day26.py
@@ -1,2 +1,11 @@
-with open()
+with open("names.txt",mode="r") as names:
+    name=names.read()
+list=name.split()
+
+with open("letter.txt",mode="r") as letters:
+    invitation=letters.read()
+print(invitation)
+
+for invite in 
+with open(f"{")    
     
