7a67457a9f3bffb27b34a1676544d849dac536ca
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-08-02 14:56:47 +0530

    automating txt files with python

--- day26/day26.py
+++ day26/day26.py
@@ -1,11 +1,22 @@
+#Editing text files with python program
+
+from os import close, replace, write
+
+
 with open("names.txt",mode="r") as names:
     name=names.read()
 list=name.split()
+print(type(list))
 
 with open("letter.txt",mode="r") as letters:
     invitation=letters.read()
 print(invitation)
 
-for invite in 
-with open(f"{")    
-    
+new_letter=[]
+for i in range(0,len(list)):
+    file=open(f"{list[i]}.txt", mode="x")
+    new_letter.append(invitation.replace("name",list[i]))
+    file.write(str(new_letter[i]))
+    file.close()
+   
+
