e6dd906312274d2f3739b5e2a1d694475af3091f
Sumit Gaur <sumit.gaur1999@gmail.com>
2021-08-07 23:30:50 +0530

    getting specific data

--- day25/day25.2.py
+++ day25/day25.2.py
@@ -1,5 +1,20 @@
 import pandas as pd
 
-df=pd.read_csv("squirrel_database")
+df=pd.read_csv("squirrel_dataset.csv")
 
-df.[]
+gray_squirrels=len(df[df["Primary Fur Color"]=="Gray"])
+print(gray_squirrels)
+
+cinnamon_squirrels=len(df[df["Primary Fur Color"]=="Cinnamon"])
+print(cinnamon_squirrels)
+
+dict={
+    "Primary Fur Color":["Gray","Cinnamon"],
+    "Number":[gray_squirrels,cinnamon_squirrels]
+}
+
+data=pd.DataFrame(dict)
+print(data)
+
+data.to_csv("Squirrels.csv")
+
